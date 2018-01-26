8082221346ea6ed40fb8fd90e44e061e3c5054b8
jjohanj <34948414+jjohanj@users.noreply.github.com>
2018-01-26 12:31:30 +0100

    Add files via upload

--- rest2.php
+++ rest2.php
@@ -6,9 +6,18 @@ $verb = $_SERVER['REQUEST_METHOD'];
 if  ($verb == "PUT") {
     $mykey = $_GET['mykey'];
     $value = $_GET['value'];
-    $allData = $mykey . " - " . $value . "</br>" ;
+    $allData = $mykey . " - " . $value . "</br>";
 
-    file_put_contents('tekst.txt', $allData, FILE_APPEND | LOCK_EX);
+    $myFile = "tekst.txt";
+    $fh = fopen($myFile, 'a');
+    fwrite($fh, $allData . "\n");
+    fclose($fh);
+
+    $read = file('tekst.txt');
+        foreach ($read as $line) {
+            echo $line .", ";
+    }
+
   }
 
  ?>
